1ace3a6f0486135110bd3b5f881fa51ca4a50e73
김희환 <167047358+kimihea@users.noreply.github.com>
2025-05-13 14:26:01 +0900

    batch

--- Assets/03.Scripts/Character/CharacterController/CharacterControllerH.cs
+++ Assets/03.Scripts/Character/CharacterController/CharacterControllerH.cs
@@ -121,6 +121,10 @@ public class CharacterControllerH : Controller
         
 
     }
+    public void AttackEvent()
+    {
+
+    }
     private void OnMouseEnter()
     {
         DetectRenderer.enabled = true;
--- Assets/03.Scripts/Character/StateMachine/CharacterNormalAttackState.cs
+++ Assets/03.Scripts/Character/StateMachine/CharacterNormalAttackState.cs
@@ -13,7 +13,7 @@ public class CharacterNormalAttackState : CharacterCombatState
     }
     public override void Update()
     {
-        stateMachine.Character.Controller.CallAttack();
+        //stateMachine.Character.Controller.CallAttack();
 
         base.Update();
     }
