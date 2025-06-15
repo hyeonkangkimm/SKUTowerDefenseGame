19d1649dc2006ba7db116700cc7c9f9ab75a2457
jubal22 <jaden030514@gmail.com>
2025-06-15 23:13:23 +0900

    대기시간에만 작동

--- Assets/03.Scripts/UI/UIButtonsONOFF.cs
+++ Assets/03.Scripts/UI/UIButtonsONOFF.cs
@@ -20,6 +20,11 @@ public class UIButtonsONOFF : MonoBehaviour
 
     public void TurnOnAndOff()
     {
+        if(!(GameManager.Instance.CombatConditionType == ECombatConditionType.START))
+        {
+            return;
+        }
+
         if (this.GetComponent<AudioSource>() != null)
         {
             this.GetComponent<AudioSource>().Play();
