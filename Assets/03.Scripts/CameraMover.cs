9db25eb13eb702583aa75309c6cd086b8c71fc38
hyeonkangkimm <kkhk75@gmail.com>
2025-06-17 02:29:46 +0900

    house

--- Assets/03.Scripts/CameraMover.cs
+++ Assets/03.Scripts/CameraMover.cs
@@ -4,6 +4,10 @@ public class CameraMover : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
+    // 이동 가능한 Z좌표 범위
+    public float minZ = 12.8f;
+    public float maxZ = 112.7f;
+
     void Update()
     {
         Vector3 direction = Vector3.zero;
@@ -17,6 +21,12 @@ public class CameraMover : MonoBehaviour
             direction = transform.right;  // 카메라 기준 오른쪽
         }
 
+        // 이동 처리
         transform.position += direction.normalized * moveSpeed * Time.deltaTime;
+
+        // Z축 위치 제한
+        Vector3 pos = transform.position;
+        pos.z = Mathf.Clamp(pos.z, minZ, maxZ);
+        transform.position = pos;
     }
 }
